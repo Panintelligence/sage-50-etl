e24ad64a86fefb5d844dfff7f508fc89ef5f1584
Dosaki <lord.g.bug@gmail.com>
2021-11-23 15:37:17 +0000

    Fixed build

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 pip install -r requirements.txt
 pyinstaller --hidden-import uuid -i ./logo.ico -F Sage50_ETL.py
-pyinstaller --hidden-import uuid-i ./logo.ico -F Sage50_Connections.py
+pyinstaller --hidden-import uuid -i ./logo.ico -F Sage50_Connections.py
